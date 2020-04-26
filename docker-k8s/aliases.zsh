d586c900f22179a9eb193396d0e287e5bc27f894
Chris Schmitz <cschmitz@skillz.com>
2020-04-25 19:23:40 -0700

    aliases

---
+++ docker-k8s/aliases.zsh
@@ -0,0 +1,5 @@
+alias .kco="vim $HOME/.kube/config"
+alias d="docker"
+alias d-c="docker-compose"
+alias dc="docker-compose"
+alias k="kubectl"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,22 +8,38 @@ alias u7="cd ../../../../../../.."
 
 alias .aco="vim $HOME/.aws/{config,credentials}"
 alias .g="vim $HOME/.gitconfig"
-alias .kco="vim $HOME/.kube/config"
 alias .sshc="vim $HOME/.ssh/config"
 alias .sshh="vim $HOME/.ssh/known_hosts"
 alias .v="vim $HOME/.vimrc"
 alias .vim="cd $HOME/.vim"
 alias .z="vim $HOME/.zshrc"
-alias d="docker"
-alias d-c="docker-compose"
-alias dc="docker-compose"
 alias dotfiles="cd ~/dotfiles"
 alias envs="env | sort"
 alias g="git"
 alias grep="grep --color"
 alias h="history"
 alias hosts="sudo $EDITOR /etc/hosts"
-alias k="kubectl"
 alias myps="ps -ef | grep $USER"
 alias s="source $HOME/.bash_profile"
 alias toxi="toxiproxy-cli"
+
+# ls
+# LC_COLLATE=C  -->  ascii sort
+if ls --color > /dev/null 2>&1; then  # GNU
+    colorflag="--color"
+else  # OSX
+    colorflag="-G"
+fi
+
+if ls --group-directories-first &> /dev/null; then
+    dirflag="--group-directories-first"
+else
+    dirflag=""
+fi
+
+alias ls="CLICOLOR_FORCE=1 ls -Flhp ${colorflag} ${dirflag}"
+alias lsa="ls -a"
+alias lsd="ls | grep '^d'"
+unset colorflag
+unset dirflag
+unset OS
