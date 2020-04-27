7c1b6e2e6a8caf058dc944a00e0ed5d81331cac0
Chris Schmitz <cschmitz@skillz.com>
2020-04-26 22:18:46 -0700

    p10k configuration

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -8,4 +8,6 @@ if ! [[ -d "$ohmyzsh_dir" ]]; then
         > oh-my-zsh-install.sh
     sh oh-my-zsh-install.sh --skip-chsh
     rm -f oh-my-zsh-install.sh
+
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH/custom/themes/powerlevel10k
 fi
