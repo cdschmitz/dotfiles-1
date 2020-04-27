1194dda840ab9ecceed02b400dddebf6fec30186
Chris Schmitz <cschmitz@skillz.com>
2020-04-27 08:45:13 -0700

    aws prompt

--- aws/config.zsh
+++ aws/config.zsh
@@ -1,2 +1,5 @@
 # Enable pager for awscli if command output extends beyond screen space
 export AWS_PAGER=less
+
+# Disable RPROMPT for aws profile in aws omz plugin (p10k already adds it)
+export SHOW_AWS_PROMPT=false
