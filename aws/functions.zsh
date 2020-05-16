59f9113ac98eda608481d92ff72f8acf739e45ad
Chris Schmitz <cschmitz@skillz.com>
2020-05-15 22:26:24 -0700

    download rds error logs

--- aws/functions.zsh
+++ aws/functions.zsh
@@ -12,6 +12,23 @@ function aws_ec2_instance_private_ip_by_tag_name() {
             --output text
 }
 
+function aws_rds_download_error_logs() {
+    db=$1
+    AWS_PAGER=
+
+    for file in $(aws rds describe-db-log-files --db-instance-identifier "$db" --filename-contains error --query 'DescribeDBLogFiles[*].LogFileName' --output text); do
+        echo "Downloading: $file"
+
+        local_filename="$(basename $file)"
+        aws rds download-db-log-file-portion \
+            --db-instance-identifier "$db" \
+            --log-file-name "$file" \
+            --starting-token 0 \
+            --output text \
+            > "$local_filename"
+    done
+}
+
 function aws_route53_get_hosted_zone_id() {
     [[ -n "$1" ]] && \
         aws route53 list-hosted-zones --query "HostedZones[?Name == '$1'].Id" --output text
