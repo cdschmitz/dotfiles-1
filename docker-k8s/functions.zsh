2b2cc309d358577efe2ac00a2c6b9cc3d5816da3
Chris Schmitz <cschmitz@skillz.com>
2020-11-12 17:06:00 -0800

    kube pod names alias

--- docker-k8s/functions.zsh
+++ docker-k8s/functions.zsh
@@ -1,3 +1,7 @@
 function docker_remove_exited() {
     docker rm -f $(docker ps -q -a -f status=exited)
 }
+
+function kubectl_get_pod_names() {
+    kubectl get pods "$@" --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'
+}
